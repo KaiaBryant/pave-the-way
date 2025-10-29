9725b647481cc87add03507218a28e76f726c5e0
Ana <anoyola17@r2happrentice.org>
2025-10-29 14:42:55 -0400

    validation survey

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
           </div>
 
           <div>
-            <a href="#" className="me-4 text-reset">
+            <a href="https://www.facebook.com" className="me-4 text-reset">
               <i className="fab fa-facebook-f"></i>
             </a>
             <a href="#" className="me-4 text-reset">
@@ -40,7 +40,7 @@ export default function Footer() {
             <div className="row mt-3">
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3"></i>Company name
+                  <i className="fas fa-gem me-3"></i>PAVE THE WAY
                 </h6>
                 <p>
                   Here you can use rows and columns to organize your footer
@@ -49,50 +49,21 @@ export default function Footer() {
                 </p>
               </div>
 
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Angular
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    React
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Vue
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Laravel
-                  </a>
-                </p>
-              </div>
-
               <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Pricing
+                    Home
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Settings
+                    Survey
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Orders
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Help
+                    Contact
                   </a>
                 </p>
               </div>
@@ -100,10 +71,10 @@ export default function Footer() {
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                  <i className="fas fa-home me-3"></i> Charlotte, NC 28202, US
                 </p>
                 <p>
-                  <i className="fas fa-envelope me-3"></i> info@example.com
+                  <i className="fas fa-envelope me-3"></i> info@pavetheway.com
                 </p>
                 <p>
                   <i className="fas fa-phone me-3"></i> + 01 234 567 88
@@ -120,9 +91,9 @@ export default function Footer() {
           className="text-center p-4"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
-          © 2021 Copyright:
-          <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-            MDBootstrap.com
+          © 2025 Copyright:
+          <a className="text-reset fw-bold" href="#">
+            PaveTheWay.com
           </a>
         </div>
       </footer>
--- client/src/pages/Survey.jsx
+++ client/src/pages/Survey.jsx
@@ -1,68 +1,149 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../styles/Survey.css";
 
 export default function Survey() {
+  // State for dropdown selections
+  const [transport, setTransport] = useState("");
+  const [time, setTime] = useState("");
+  const [day, setDay] = useState("");
+  const [toZip, setToZip] = useState("");
+  const [fromZip, setFromZip] = useState("");
+  // Handle individual dropdowns
+  const handleTransportClick = (e) => setTransport(e.target.value);
+  const handleTimeClick = (e) => setTime(e.target.value);
+  const handleDayClick = (e) => setDay(e.target.value);
+  // Navigate to the Results Page
+  const navigate = useNavigate();
+
+  // Handle form submission logic here
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const zipRegex = /^\d{5}$/;
+    if (!transport || !time || !day || !toZip || !fromZip) {
+      alert("Please fill in all fields before submitting");
+    }
+    if (!zipRegex.test(toZip) || !zipRegex.test(fromZip)) {
+      alert("Zipcodes must be 5 digits");
+      return;
+    } else if (
+      transport === "select" ||
+      time === "select" ||
+      day === "select" ||
+      toZip === "select" ||
+      fromZip === "select"
+    ) {
+      console.log("Error: Please make valid selections for all fields");
+    } else {
+      navigate("/result", {
+        state: { transport, time, day, toZip, fromZip },
+      });
+      console.log({ transport, time, day, toZip, fromZip }); // Print out results
+    }
+  };
   return (
     <div>
-      <form>
-        <label htmlFor="transport">Transportation:</label>
-        <br />
-        <select id="transport" name="transport">
-          <option value="bike">Bus</option>
-          <option value="bike">Bike</option>
-          <option value="car">Car</option>
-          <option value="walk">Walk</option>
-          <option value="rail">Rail</option>
-        </select>
-        <br />
-        <label htmlFor="fname">Time of day:</label>
-        <br />
-        <select id="transport" name="transport">
-          <option value="12AM">12AM</option>
-          <option value="1AM">1AM</option>
-          <option value="2AM">2AM</option>
-          <option value="3AM">3AM</option>
-          <option value="4AM">4AM</option>
-          <option value="5AM">5AM</option>
-          <option value="6AM">6AM</option>
-          <option value="7AM">7AM</option>
-          <option value="8AM">8AM</option>
-          <option value="9AM">9AM</option>
-          <option value="10AM">10AM</option>
-          <option value="11AM">11AM</option>
-          <option value="12PM">12PM</option>
-          <option value="1PM">1PM</option>
-          <option value="2PM">2PM</option>
-          <option value="3PM">3PM</option>
-          <option value="4PM">4PM</option>
-          <option value="5PM">5PM</option>
-          <option value="6PM">6PM</option>
-          <option value="7PM">7PM</option>
-          <option value="8PM">8PM</option>
-          <option value="9PM">9PM</option>
-          <option value="10PM">10PM</option>
-          <option value="11PM">11PM</option>
-        </select>
-        <br />
+      <section className="survey-form-section">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="transport-form">Transportation:</label>
+          <select
+            id="transport-form"
+            name="transport"
+            className="survey-form__select"
+            value={transport}
+            onChange={handleTransportClick}
+          >
+            <option value="select">Please select a transport</option>
+            <option value="bus">Buses</option>
+            <option value="bike">Bicycles</option>
+            <option value="car">Cars</option>
+            <option value="walk">Walk</option>
+            <option value="rail">Rail</option>
+          </select>
 
-        <label htmlFor="weekOfDay">Day of the week:</label>
-        <br />
-        <select id="weekOfDay" name="weekOfDay">
-          <option value="sunday">Sunday</option>
-          <option value="monday">Monday</option>
-          <option value="tuesday">Tuesday</option>
-          <option value="wednesday">Wednesday</option>
-          <option value="thursday">Thursday</option>
-          <option value="friday">Friday</option>
-          <option value="saturday">Saturday</option>
-        </select>
-        <br />
+          <label htmlFor="time-form">Time of Day:</label>
+          <select
+            id="time-form"
+            name="time"
+            className="survey-form__select"
+            value={time}
+            onChange={handleTimeClick}
+          >
+            <option value="select">Please select the time of day</option>
+            <option value="12AM">12 AM</option>
+            <option value="1AM">1 AM</option>
+            <option value="2AM">2 AM</option>
+            <option value="3AM">3 AM</option>
+            <option value="4AM">4 AM</option>
+            <option value="5AM">5 AM</option>
+            <option value="6AM">6 AM</option>
+            <option value="7AM">7 AM</option>
+            <option value="8AM">8 AM</option>
+            <option value="9AM">9 AM</option>
+            <option value="10AM">10 AM</option>
+            <option value="11AM">11 AM</option>
+            <option value="12PM">12 PM</option>
+            <option value="1PM">1 PM</option>
+            <option value="2PM">2 PM</option>
+            <option value="3PM">3 PM</option>
+            <option value="4PM">4 PM</option>
+            <option value="5PM">5 PM</option>
+            <option value="6PM">6 PM</option>
+            <option value="7PM">7 PM</option>
+            <option value="8PM">8 PM</option>
+            <option value="9PM">9 PM</option>
+            <option value="10PM">10 PM</option>
+            <option value="11PM">11 PM</option>
+          </select>
+          <br />
 
-        <label htmlFor="destination">Zip-Code:</label>
-        <br />
-        <input type="to" id="start-zip" placeholder="To" />
-        <input type="from" id="end-zip" placeholder="From" />
-      </form>
+          <label htmlFor="day-form">Week of Day:</label>
+          <select
+            id="day-form"
+            name="day"
+            className="survey-form__select"
+            value={day}
+            onChange={handleDayClick}
+          >
+            <option value="select">Please select the day of the week</option>
+            <option value="sunday">Sunday</option>
+            <option value="monday">Monday</option>
+            <option value="tuesday">Tuesday</option>
+            <option value="wednesday">Wednesday</option>
+            <option value="thursday">Thursday</option>
+            <option value="friday">Friday</option>
+            <option value="saturday">Saturday</option>
+          </select>
+          <br />
+
+          <label htmlFor="to-zip-form">To Zipcode:</label>
+          <input
+            type="text"
+            id="to-zip"
+            placeholder="To"
+            value={toZip}
+            pattern="\d{5}"
+            title="Please enter a 5-digit zipcode"
+            onChange={(e) => setToZip(e.target.value)}
+          />
+
+          <label htmlFor="from-zip-form">From Zipcode:</label>
+          <input
+            type="text"
+            id="from-zip"
+            placeholder="From"
+            value={fromZip}
+            pattern="\d{5}"
+            title="Please enter a 5-digit zipcode"
+            onChange={(e) => setFromZip(e.target.value)}
+          />
+
+          <button type="submit" className="survey-form__button">
+            Submit
+          </button>
+        </form>
+      </section>
     </div>
   );
 }
